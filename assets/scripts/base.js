3ab319d28811a2f0991aa5b724e2128b90310f86
SoRaang <dsc0320@hotmail.com>
2024-07-25 17:11:20 +0900

    Update base.js

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -66,3 +66,56 @@ txtTodoInput.addEventListener('keyup', (e) => {
         addTodo();
     }
 });
+
+
+
+// 객체 다루기, class를 이용하여 객체 만들기
+
+const studentArray = [
+    {
+        name: '김창완',
+        korean: 95,
+        english: 89,
+        math: 2,
+        science: 78
+    }
+];
+
+let printOut = '이름\t총점\t평균점수\n------------------------------\n';
+
+function getSumOf(person) {
+    return person.korean + person.english + person.math + person.science;
+}
+
+function getAverageOf(person) {
+    return getSumOf(person) / 4;
+}
+
+for (const person of studentArray) {
+    printOut += `${person.name}\t${getSumOf(person)}점\t${getAverageOf(person)}점\n`
+}
+
+console.log(printOut);
+
+class makeStudents {
+    constructor(stName, stKorean, stEnglish, stMath, stScience) {
+        this.name = stName,
+        this.ptKorean = stKorean,
+        this.ptEnglish = stEnglish,
+        this.ptMath = stMath,
+        this.ptScience = stScience
+    }
+
+    print() {
+        let printOut = '이름\t총점\t평균점수\n------------------------------\n';
+        let totalPoints = this.ptKorean + this.ptEnglish + this.ptMath + this.ptScience
+
+        printOut += `${ this.name }\t${ totalPoints }점\t${ Math.floor(totalPoints / 4) }점`;
+
+        console.log(printOut);
+    }
+}
+
+const normalMan = new makeStudents('김창완', 95, 89, 2, 75);
+
+normalMan.print();
